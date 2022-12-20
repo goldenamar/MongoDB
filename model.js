29fa4e1d114ae68de21dc4c04c975dad0c30e4b1
Amarchingun <Blackberry@nest-56s-iMac.local>
2022-12-20 19:10:22 +0800

    Mongodb first lesson

---
+++ model.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require("mongoose");
+
+const UserScheme = new Schema({
+    firstname: String,
+    lastname: String,
+    registerdate: {type: Date, default: Date.now},
+});
+
+const UserModel = model("User", UserScheme);
+
+module.exports = UserModel;
